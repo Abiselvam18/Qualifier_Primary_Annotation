e9a6b6eaa10b714a39d67ee242dcf88f78441a4c
Abinaya <abiselvam080@gmail.com>
2022-05-08 15:42:14 +0530

    commit 58

---
+++ Qualifier_Primary_Annotation/src/main/java/com/edu/MediaTek.java
@@ -0,0 +1,15 @@
+package com.edu;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Primary
+public class MediaTek implements MobileProcessor {
+
+	public void processor() {
+		System.out.println("World Second Best Processor");
+		
+	}
+
+}
